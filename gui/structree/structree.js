b6047b230ff42be3f8af29816d3bc533f5bad6e5
s0600204 <dev@s0600204>
2014-11-09 23:36:33 +0000

    Implement start of the data parsing
    * So far
      - Fetch starting units from JSON file of selected civ
      - Load units, and fetch the structures they can build
      - Load structures, and fetch the units they can train
      - If new units found, load these new units and fetch the structures they can build
      - If new structures found, return to point 3, else continue
    * Still to do:
      - Include templates' parents
      - Fetch technology lists

--- gui/structree/structree.js
+++ gui/structree/structree.js
@@ -1,6 +1,8 @@
 
-var g_Data = {};
+var g_ParsedData = {};
+var g_Lists = {};
 var g_CivData = {};
+var g_SelectedCiv = "";
 
 function init (settings)
 {
@@ -32,11 +34,85 @@ function initCivNameList ()
 function selectCiv (civCode)
 {
 	warn(civCode + " has been selected in the structree page");
+	g_SelectedCiv = civCode;
 	
-	// todo: do something
+	g_ParsedData["units"] = {};
+	g_ParsedData["structures"] = {};
+	g_Lists["units"] = [];
+	g_Lists["structures"] = [];
 	
+	// get initial units
+	for (var entity of g_CivData[civCode].StartEntities)
+	{
+		if (entity.Template.slice(0, 5) == "units")
+			g_Lists.units.push(entity.Template);
+	}
 	
-	
+	do {
+		for (var u of g_Lists.units)
+		{
+			if (!g_ParsedData.units[u])
+				g_ParsedData.units[u] = loadUnit(u);
+		}
 		
+		for (var s of g_Lists.structures)
+		{
+			if (!g_ParsedData.structures[s])
+				g_ParsedData.structures[s] = loadStructure(s);
+		}
+	} while (Object.keys(g_ParsedData.units).length < g_Lists.units.length);
+	
+}
+
+function loadUnit (unitCode)
+{
+	var unitInfo = loadTemplate(unitCode);
+	
+	var unit = {
+			"genericName": unitInfo.Identity.GenericName
+		,	"specificName": unitInfo.Identity.SpecificName
+		,	"icon": unitInfo.Identity.Icon
+	};
+	
+	if (unitInfo.Builder)
+	{
+		for (var build of unitInfo.Builder.Entities)
+		{
+			if (build.charAt(0) == "-")
+				continue; // temporary fix, until I get tokens parsing properly up and done
+			
+			build = build.replace("{civ}", g_SelectedCiv);
+			if (g_Lists.structures.indexOf(build) < 0)
+				g_Lists.structures.push(build);
+		}
+	}
+	
+	return unit;
+}
+
+function loadStructure (structCode)
+{
+	var structInfo = loadTemplate(structCode);
+	
+	var structure = {
+			"genericName": structInfo.Identity.GenericName
+		,	"specificName": (structInfo.Identity.SpecificName) ? structInfo.Identity.SpecificName : "?"
+		,	"icon": structInfo.Identity.Icon
+	}
+	
+	if (structInfo.ProductionQueue && structInfo.ProductionQueue.Entities)
+	{
+		for (var build of structInfo.ProductionQueue.Entities)
+		{
+			if (build.charAt(0) == "-")
+				continue; // temporary fix, until I get tokens parsing properly up and done
+			
+			build = build.replace("{civ}", g_SelectedCiv);
+			if (g_Lists.units.indexOf(build) < 0)
+				g_Lists.units.push(build);
+		}
+	}
+	
+	return structure;
 }
 
