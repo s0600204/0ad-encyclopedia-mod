e4d1929ad3f4403d1b3b6e5d3499d11aff9cef73
leper <leper@wildfiregames.com>
2014-12-25 23:35:36 +0100

    Use AE spelling, fix placing of menu.
    * Remove placeholders instead of translating them.
    * Use default parameter.

--- gui/common/functions_civinfo.js
+++ gui/common/functions_civinfo.js
@@ -8,23 +8,22 @@
 // ====================================================================
 
 
-function loadCivData (playableOnly)
+function loadCivData (playableOnly = false)
 {
 	// Load all JSON files containing civ data
 	var civData = {};
 	var civFiles = Engine.BuildDirEntList("civs/", "*.json", false);
-	playableOnly = (playableOnly === undefined) ? false : true;
-	
+
 	for (let filename of civFiles)
 	{
 		// Parse data if valid file
 		let data = parseJSONData(filename);
 		translateObjectKeys(data, ["Name", "Description", "History", "Special"]);
-		
+
 		if (!playableOnly || data.SelectableInGameSetup)
 			civData[data.Code] = data;
 	}
-	
+
 	return civData;
 }
 
